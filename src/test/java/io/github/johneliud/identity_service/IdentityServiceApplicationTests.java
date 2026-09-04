61b16bd339feba851698b692291072a94785d0bd
John <johneliud4@gmail.com>
2026-09-04 21:19:39 +0300

    initial commit.

---
+++ src/test/java/io/github/johneliud/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johneliud.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
