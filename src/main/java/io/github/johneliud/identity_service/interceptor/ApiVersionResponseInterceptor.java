3d7f392cdb4211242ae4eea7693f822ff2934b64
John <johneliud4@gmail.com>
2026-09-06 19:08:39 +0300

    feat: add API version response interceptor to include X-API-Version header.

---
+++ src/main/java/io/github/johneliud/identity_service/interceptor/ApiVersionResponseInterceptor.java
@@ -0,0 +1,40 @@
+package io.github.johneliud.identity_service.interceptor;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.web.accept.ApiVersionStrategy;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ApiVersionResponseInterceptor implements HandlerInterceptor {
+
+    static final String RESPONSE_HEADER = "X-API-Version";
+
+    private final ApiVersionStrategy apiVersionStrategy;
+    private final String defaultVersion;
+
+    public ApiVersionResponseInterceptor(ApiVersionStrategy apiVersionStrategy, String defaultVersion) {
+        this.apiVersionStrategy = apiVersionStrategy;
+        this.defaultVersion = defaultVersion;
+    }
+
+    @Override
+    public boolean preHandle(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler) {
+
+        String version = apiVersionStrategy.resolveVersion(request);
+
+        if (version == null) {
+            version = defaultVersion;
+        }
+
+        if (version != null) {
+            response.setHeader(RESPONSE_HEADER, version);
+        }
+
+        return true;
+    }
+}
