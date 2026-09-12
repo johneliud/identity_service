ded7359c3c19621dcf58d920312989090afdc4cf
John <johneliud4@gmail.com>
2026-09-12 09:39:54 +0300

    add: LoginResponse DTO for authentication tokens.

---
+++ src/main/java/io/github/johneliud/identity_service/dto/LoginResponse.java
@@ -0,0 +1,17 @@
+package io.github.johneliud.identity_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private long expiresIn;
+}
